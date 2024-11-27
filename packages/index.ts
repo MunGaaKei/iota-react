1f3dae0f71d92d9396a2e8bc186085ccb557cd17
Iann Man <502975759@qq.com>
2024-11-27 15:43:21 +0800

    init: types

--- packages/index.ts
+++ packages/index.ts
@@ -34,6 +34,48 @@ import Tree from "./components/tree";
 import Upload from "./components/upload";
 import Video from "./components/video";
 import usePreview from "./js/usePreview";
+import useRipple from "./js/useRipple";
+
+import { IAffix } from "./components/affix/type";
+import { IBadge } from "./components/badge/type";
+import { IButton, IButtonToggle } from "./components/button/type";
+import { ICard } from "./components/card/type";
+import { ICheckbox, ICheckboxItem } from "./components/checkbox/type";
+import { ICollapse, ICollapseItem } from "./components/collapse/type";
+import { IDatagrid } from "./components/datagrid/type";
+import { IDatePicker } from "./components/datepicker/type";
+import { IDescription } from "./components/description/type";
+import { IDrawer } from "./components/drawer/type";
+import { IDropdown, IDropItem } from "./components/dropdown/type";
+import { IFlex } from "./components/flex/type";
+import { IField, IForm } from "./components/form/type";
+import { IIcon } from "./components/icon/type";
+import { IImage, IImageList } from "./components/image/type";
+import {
+	IInput,
+	IInputNumber,
+	IInputRange,
+	ITextarea,
+} from "./components/input/type";
+import { IList, IListItem } from "./components/list/type";
+import { ILoading } from "./components/loading/type";
+import { IMessage, IMessageItem } from "./components/message/type";
+import { IModal } from "./components/modal/type";
+import { IPagination } from "./components/pagination/type";
+import { IPopconfirm } from "./components/popconfirm/type";
+import { IPopup } from "./components/popup/type";
+import { IProgress } from "./components/progress/type";
+import { IRadio, IRadioItem } from "./components/radio/type";
+import { ISelect, ISelectOptions } from "./components/select/type";
+import { IStep, IStepItem } from "./components/step/type";
+import { ISwiper, ISwiperItem } from "./components/swiper/type";
+import { ITabItem, ITabs } from "./components/tabs/type";
+import { ITag } from "./components/tag/type";
+import { IText, ITextNumber, ITextTime } from "./components/text/type";
+import { ITree, ITreeItem } from "./components/tree/type";
+import { IUpload, IUploadItem } from "./components/upload/type";
+import { IVideo } from "./components/video/type";
+import { IPreview } from "./js/usePreview/type";
 
 export {
 	Affix,
@@ -70,6 +112,65 @@ export {
 	Text,
 	Tree,
 	Upload,
-	Video,
 	usePreview,
+	useRipple,
+	Video,
+};
+
+export type {
+	IAffix,
+	IBadge,
+	IButton,
+	IButtonToggle,
+	ICard,
+	ICheckbox,
+	ICheckboxItem,
+	ICollapse,
+	ICollapseItem,
+	IDatagrid,
+	IDatePicker,
+	IDescription,
+	IDrawer,
+	IDropdown,
+	IDropItem,
+	IField,
+	IFlex,
+	IForm,
+	IIcon,
+	IImage,
+	IImageList,
+	IInput,
+	IInputNumber,
+	IInputRange,
+	IList,
+	IListItem,
+	ILoading,
+	IMessage,
+	IMessageItem,
+	IModal,
+	IPagination,
+	IPopconfirm,
+	IPopup,
+	IPreview,
+	IProgress,
+	IRadio,
+	IRadioItem,
+	ISelect,
+	ISelectOptions,
+	IStep,
+	IStepItem,
+	ISwiper,
+	ISwiperItem,
+	ITabItem,
+	ITabs,
+	ITag,
+	IText,
+	ITextarea,
+	ITextNumber,
+	ITextTime,
+	ITree,
+	ITreeItem,
+	IUpload,
+	IUploadItem,
+	IVideo,
 };
